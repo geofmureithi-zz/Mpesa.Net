9b3275f84ea6a90544404c9020a179fae3655edf
Geoffrey Mureithi <mureithinjuguna@gmail.com>
2019-01-01 11:00:47 +0300

    Attempting to fix Instance error

--- MpesaTests/MpesaTests/ApiTest.cs
+++ MpesaTests/MpesaTests/ApiTest.cs
@@ -1,7 +1,7 @@
 using Xunit;
 using Safaricom.Mpesa;
-using static Safaricom.Mpesa.Api;
 using Safaricom.Mpesa.Helpers;
+using static Safaricom.Mpesa.Api;
 
 namespace MpesaTests
 {
@@ -9,7 +9,7 @@ namespace MpesaTests
     {
         private static readonly string FakeURL = "https://example.co.ke/url";
         private static readonly long msisdn = 254712345678;
-        public Api mpesa;
+
         private static readonly ExtraConfig configs = new ExtraConfig
         {
             ShortCode = 600111,
@@ -20,22 +20,22 @@ namespace MpesaTests
             CertPath = "~/c-sharp-mpesa-lib/Mpesa/cert.cer"
         };
 
-        public ApiTest()
+        public Api GetApi()
         {
-            mpesa = new Api(Env.Sandbox, "OT13kmfq1I8GcD2D4JIcyrHO7C3IAM81", "nxKU4f4Zq6h1urLD", configs);
+            return new Api(Env.Sandbox, "OT13kmfq1I8GcD2D4JIcyrHO7C3IAM81", "nxKU4f4Zq6h1urLD", configs);
         }
 
 
         [Fact]
         public void AuthTest()
         {
-            Assert.NotEmpty(mpesa.Auth().Data.AccessToken);
+            Assert.NotEmpty(GetApi().Auth().Data.AccessToken);
         }
 
         [Fact]
         public async void AsyncAuthTest()
         {
-            var res = await mpesa.AuthAsync();
+            var res = await GetApi().AuthAsync();
             Assert.NotEmpty(res.Data.AccessToken);
         }
 
@@ -43,50 +43,50 @@ namespace MpesaTests
         public async void AccountBalance()
         {
             var party = new IdentityParty(configs.ShortCode, IdentityParty.IdentifierType.SHORTCODE);
-            var res = await mpesa.AccountBalance(party, FakeURL, FakeURL);
+            var res = await GetApi().AccountBalance(party, FakeURL, FakeURL);
             Assert.Equal(0, res.Data.ResponseCode);
         }
         [Fact]
         public async void C2BRegister()
         {
-            var res = await mpesa.C2BRegister(FakeURL, FakeURL);
+            var res = await GetApi().C2BRegister(FakeURL, FakeURL);
             Assert.NotNull(res.Data.ConversationId);
         }
         [Fact]
         public async void B2B()
         {
             var receiverParty = new IdentityParty(600111, IdentityParty.IdentifierType.SHORTCODE);
-            var res = await mpesa.B2B(receiverParty, 100, FakeURL, FakeURL);
+            var res = await GetApi().B2B(receiverParty, 100, FakeURL, FakeURL);
             Assert.NotNull(res.Data.ConversationId);
         }
 
         [Fact]
         public async void C2BSimulate()
         {
-            var res = await mpesa.C2BSimulate(msisdn, 100, "Sample Ref");
+            var res = await GetApi().C2BSimulate(msisdn, 100, "Sample Ref");
             Assert.NotNull(res.Data.ConversationId);
         }
 
         [Fact]
         public async void B2C()
         {
-            var res = await mpesa.B2C(msisdn, 100, FakeURL, FakeURL);
+            var res = await GetApi().B2C(msisdn, 100, FakeURL, FakeURL);
             Assert.NotNull(res.Data.ConversationId);
         }
 
         [Fact]
         public async void LipaNaMpesa()
         {
-            var res = await mpesa.LipaNaMpesaOnline(msisdn, 100, FakeURL, "Some Ref");
+            var res = await GetApi().LipaNaMpesaOnline(msisdn, 100, FakeURL, "Some Ref");
             Assert.NotNull(res.Data.CheckoutRequestID);
-            var requestDetails = await mpesa.LipaNaMpesaQuery(res.Data.CheckoutRequestID);
+            var requestDetails = await GetApi().LipaNaMpesaQuery(res.Data.CheckoutRequestID);
             Assert.NotNull(requestDetails.Data.ResponseCode);
         }
 
         [Fact]
         public async void ReversalRequest()
         {
-            var res = await mpesa.ReversalRequest("LKXXXX1234", 100, FakeURL, FakeURL);
+            var res = await GetApi().ReversalRequest("LKXXXX1234", 100, FakeURL, FakeURL);
             Assert.Equal(0, res.Data.ResponseCode);
 
         }
@@ -94,7 +94,7 @@ namespace MpesaTests
         [Fact]
         public async void TransactionStatus()
         {
-            var res = await mpesa.TransactionStatus("LKXXXX1234", FakeURL, FakeURL);
+            var res = await GetApi().TransactionStatus("LKXXXX1234", FakeURL, FakeURL);
             Assert.Equal(0, res.Data.ResponseCode);
         }
     }
